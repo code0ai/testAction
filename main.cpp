1499bd62bed1effcc58ad5a8de8e82f355228a31
code0ai <data4machine2ai@gmail.com>
2025-04-20 15:42:42 +0530

    change

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "This is a C++ program." << std::endl;
+    std::cout << "Let's learn C++ together!" << std::endl;
     return 0;
 }
